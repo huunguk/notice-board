2999637d2c78e4785bdc0650ae7b43d272885840
huunguk <huunguk@gmail.com>
2023-02-22 16:12:16 +0900

    Vue Router v4.x

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheHeader from './latyouts/TheHeader.vue';
+import TheView from './latyouts/TheView.vue';
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+  <TheView></TheView>
+</template>
+
+<style lang="scss" scoped></style>
